e7c38a5eff818a6aaecc97c2b919d46387949b64
jblomquist2 <159498283+jblomquist2@users.noreply.github.com>
2024-09-23 00:11:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+testScore = int(input("Enter your test score: "))
+classRank = int(input("Enter your class rank: "))
+
+if testScore >= 90:
+  if classRank >= 25:
+    print("Accept")
+  else:
+    print("Reject")
+else:
+  if testScore >= 80:
+    if classRank >= 50:
+      print("Accept")
+    else:
+      print("Reject")
+  else:
+    if testScore >= 70:
+      if classRank >= 75:
+        print("Accept")
+      else:
+        print("Reject")
+    else:
+      print("Reject")
